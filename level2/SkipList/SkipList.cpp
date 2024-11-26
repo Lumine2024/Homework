1b85b6854d8d9725a1d5d1e7b21ffd4c9da6e9e5
Lumine2024 <149254783+Lumine2024@users.noreply.github.com>
2024-11-26 22:25:25 +0800

    更新 SkipList.cpp

--- level2/SkipList/SkipList.cpp
+++ level2/SkipList/SkipList.cpp
@@ -9,18 +9,19 @@ using namespace std;
 #else
 #define PRIVATE private
 #endif
-template<class _Ty, class _Pr = less<_Ty>, class _Eq = equal_to<_Ty>> class skiplist {
+template<class _Ty, class _Pr = less<_Ty>, class _Eq = equal_to<_Ty>>
+class skiplist {
 public:
     using value_type = _Ty;
-    using pointer = _Ty *;
-    using reference = _Ty &;
-    using const_reference = const _Ty &;
+    using pointer = _Ty*;
+    using reference = _Ty&;
+    using const_reference = const _Ty&;
     using size_type = size_t;
 PRIVATE:
-    template<class __Ty> struct _Node {
-        __Ty _Myval;
-        vector<_Node<__Ty> *> _Forward;
-        _Node(int _Val, int _Level) :_Myval(_Val), _Forward(_Level + 1, nullptr) {}
+    struct _Node {
+        value_type _Myval;
+        vector<_Node*> _Forward;
+        _Node(int _Val, int _Level) : _Myval(_Val), _Forward(_Level + 1, nullptr) {}
         ~_Node() = default;
     };
     struct _Gen_bool {
@@ -35,38 +36,38 @@ PRIVATE:
         }
     };
 public:
-    skiplist() : _Nodecnt(0), _Level(0), _Dummy(new _Node<value_type>(0, max_level)) {}
-    skiplist(const initializer_list<value_type> &_Ilist) : _Nodecnt(_Ilist.size())
-        , _Dummy(new _Node<value_type>(0, max_level)) {
-        for(const auto &elem : _Ilist) {
+    skiplist() : _Nodecnt(0), _Level(0), _Dummy(new _Node(0, max_level)) {}
+    skiplist(const initializer_list<value_type>& _Ilist)
+        : _Nodecnt(_Ilist.size()), _Dummy(new _Node(0, max_level)) {
+        for (const auto& elem : _Ilist) {
             insert(elem);
         }
     }
-	skiplist(const skiplist<_Ty, _Pr, _Eq> &other) :_Nodecnt(other._Nodecnt)
-		, _Dummy(new _Node<value_type>(0, max_level)), _Controller(other._Controller)
-		, _Compare(other._Compare), _Equal(other._Equal) {
-		_Node<value_type> *curr = other._Dummy->_Forward[0];
-		while(curr) {
-			insert(curr->_Myval);
-			curr = curr->_Forward[0];
-		}
-	}
+    skiplist(const skiplist<_Ty, _Pr, _Eq>& other) : _Nodecnt(other._Nodecnt),
+        _Dummy(new _Node(0, max_level)), _Controller(other._Controller),
+        _Compare(other._Compare), _Equal(other._Equal) {
+        _Node* curr = other._Dummy->_Forward[0];
+        while (curr) {
+            insert(curr->_Myval);
+            curr = curr->_Forward[0];
+        }
+    }
     ~skiplist() {
-        _Node<value_type> *curr = _Dummy;
-        while(curr) {
-            _Node<value_type> *tmp = curr;
+        _Node* curr = _Dummy;
+        while (curr) {
+            _Node* tmp = curr;
             curr = curr->_Forward[0];
             delete tmp;
         }
     }
     bool search(const_reference _Val) const {
-        _Node<value_type> *curr = _Dummy;
-        for(int i = static_cast<int>(_Level); i >= 0; --i) {
-            while(curr->_Forward[i]) {
-                if(_Equal(_Val, curr->_Forward[i]->_Myval)) {
+        _Node* curr = _Dummy;
+        for (int i = static_cast<int>(_Level); i >= 0; --i) {
+            while (curr->_Forward[i]) {
+                if (_Equal(_Val, curr->_Forward[i]->_Myval)) {
                     return true;
                 }
-                else if(_Compare(curr->_Forward[i]->_Myval, _Val)) {
+                else if (_Compare(curr->_Forward[i]->_Myval, _Val)) {
                     curr = curr->_Forward[i];
                 }
                 else {
@@ -77,11 +78,11 @@ public:
         return false;
     }
     void insert(const_reference _Val) {
-        vector<_Node<value_type> *> update(max_level + 1, nullptr);
-        _Node<value_type> *curr = _Dummy;
-        for(int i = static_cast<int>(_Level); i >= 0; --i) {
-            while(curr->_Forward[i]) {
-                if(_Compare(curr->_Forward[i]->_Myval, _Val)) {
+        vector<_Node*> update(max_level + 1, nullptr);
+        _Node* curr = _Dummy;
+        for (int i = static_cast<int>(_Level); i >= 0; --i) {
+            while (curr->_Forward[i]) {
+                if (_Compare(curr->_Forward[i]->_Myval, _Val)) {
                     curr = curr->_Forward[i];
                 }
                 else {
@@ -91,35 +92,35 @@ public:
             update[i] = curr;
         }
         size_t level = _Get_random_level();
-        if(level > _Level) {
+        if (level > _Level) {
             level = _Level + 1;
             update[level] = _Dummy;
             _Level = level;
         }
-        _Node<value_type> *newnode = new _Node<value_type>(_Val, level);
-        for(size_t i = 0; i <= level; ++i) {
+        _Node* newnode = new _Node(_Val, level);
+        for (size_t i = 0; i <= level; ++i) {
             newnode->_Forward[i] = update[i]->_Forward[i];
             update[i]->_Forward[i] = newnode;
         }
         ++_Nodecnt;
     }
     bool erase(const_reference _Val) {
-        _Node<value_type> *curr = _Dummy;
-        for(int i = static_cast<int>(_Level); i >= 0; --i) {
-            while(curr->_Forward[i]) {
-                if(_Equal(_Val, curr->_Forward[i]->_Myval)) {
-					_Node<value_type> *to_erase = curr->_Forward[i];
-                    for(int j = i; j >= 0; --j) {
-						while(!_Equal(curr->_Forward[j]->_Myval, _Val)) {
-							curr = curr->_Forward[j];
-						}
-						curr->_Forward[j] = to_erase->_Forward[j];
+        _Node* curr = _Dummy;
+        for (int i = static_cast<int>(_Level); i >= 0; --i) {
+            while (curr->_Forward[i]) {
+                if (_Equal(_Val, curr->_Forward[i]->_Myval)) {
+                    _Node* to_erase = curr->_Forward[i];
+                    for (int j = i; j >= 0; --j) {
+                        while (!_Equal(curr->_Forward[j]->_Myval, _Val)) {
+                            curr = curr->_Forward[j];
+                        }
+                        curr->_Forward[j] = to_erase->_Forward[j];
                     }
-					delete to_erase;
-					--_Nodecnt;
+                    delete to_erase;
+                    --_Nodecnt;
                     return true;
                 }
-                else if(_Compare(curr->_Forward[i]->_Myval, _Val)) {
+                else if (_Compare(curr->_Forward[i]->_Myval, _Val)) {
                     curr = curr->_Forward[i];
                 }
                 else {
@@ -136,10 +137,10 @@ public:
         return _Level;
     }
     void display() const {
-        for(int i = static_cast<int>(_Level); i >= 0; --i) {
+        for (int i = static_cast<int>(_Level); i >= 0; --i) {
             cout << "level " << i << ": ";
-            _Node<value_type> *node = _Dummy->_Forward[i];
-            while(node != nullptr) {
+            _Node* node = _Dummy->_Forward[i];
+            while (node != nullptr) {
                 cout << node->_Myval << " ";
                 node = node->_Forward[i];
             }
@@ -149,13 +150,13 @@ public:
     }
 PRIVATE:
     size_t _Get_random_level() {
-        for(size_t ret = 0; ret < max_level; ++ret) {
-            if(!_Controller._Getbool()) return ret;
+        for (size_t ret = 0; ret < max_level; ++ret) {
+            if (!_Controller._Getbool()) return ret;
         }
         return max_level - 1;
     }
 PRIVATE:
-    _Node<value_type> *_Dummy;
+    _Node* _Dummy;
     size_type _Level, _Nodecnt;
     _Gen_bool _Controller;
     _Pr _Compare;
